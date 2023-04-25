a8087735f84dc9f4aac46d98e099bb0f2c4e0bb3
lilian.schall <lilian.schall@epita.fr>
2023-04-25 19:10:30 +0200

    fix: fixed the example model architecture to learn the xor function

--- examples/xor/src/main.rs
+++ examples/xor/src/main.rs
@@ -9,18 +9,18 @@ use bricks::shapes::dense_shape::DenseShape;
 
 fn main() {
 
-    let activations = vec![DenseActivation::Sigmoid, DenseActivation::Sigmoid];
-    let shape = vec![DenseShape::new(2,0,0), DenseShape::new(3,0,0), DenseShape::new(1,0,0)];
+    let activations = vec![DenseActivation::Relu, DenseActivation::Tanh];
+    let shape = vec![DenseShape::new(2,1,1), DenseShape::new(3,1,1), DenseShape::new(1,1,1)];
     let mut model = DenseModel::new(activations, Loss::MeanSquaredError, shape, None);
 
     let training_data = vec![
-        (Matrix::from(vec![1.0,0.0]), Matrix::from(vec![0.0])),
-        (Matrix::from(vec![1.0,0.0]), Matrix::from(vec![0.0])),
-        (Matrix::from(vec![1.0,0.0]), Matrix::from(vec![0.0])),
-        (Matrix::from(vec![1.0,0.0]), Matrix::from(vec![0.0]))];
+        (Matrix::from(vec![1.0,0.0]), Matrix::from(vec![1.0])),
+        (Matrix::from(vec![0.0,1.0]), Matrix::from(vec![1.0])),
+        (Matrix::from(vec![1.0,1.0]), Matrix::from(vec![0.0])),
+        (Matrix::from(vec![0.0,0.0]), Matrix::from(vec![0.0]))];
     let testing_data = training_data.clone();
 
-    let mut session = Session::new(model, 1E-2, training_data, testing_data, 200, None);
+    let mut session = Session::new(model, 1E-2, training_data, testing_data, 50000, Some(0.005));
 
     session.fit();
     model = session.give_model();
